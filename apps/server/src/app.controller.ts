934766d12141e1bbe224ede5a7910152034a334b
comeintovlanc <rkdtjdwns916@gmail.com>
2023-11-11 16:55:26 +0900

    [feat]: .env file is activate in apps/server

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as process from 'process';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly configService :ConfigService) {}
 
   @Get()
   getHello(): string {
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
