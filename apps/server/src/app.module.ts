978ed8a820a1fe7caaf10629a9d97011d078532a
comeintovlanc <rkdtjdwns916@gmail.com>
2023-11-24 11:12:11 +0900

    [chore]: .gitignore fixed

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -7,7 +7,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '../../.env.local',
+      envFilePath: '.env.local',
     }),
   ],
   controllers: [AppController],
