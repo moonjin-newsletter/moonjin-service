fad7c514df45d9dab56926b4c8cfa2694b66f0ac
comeintovlanc <rkdtjdwns916@gmail.com>
2024-01-03 16:06:06 +0900

    [fix] swagger build error fixed

--- apps/server/src/swagger/swaggerBoot.ts
+++ apps/server/src/swagger/swaggerBoot.ts
@@ -4,6 +4,11 @@ import { readFileSync } from "fs";
 import * as path from "path";
 
 export function swaggerBoot(app: INestApplication<any>) {
-  const swaagerConfig = readFileSync(path.join(__dirname, '../../../swagger.json'), 'utf8');
-  SwaggerModule.setup('swagger', app, JSON.parse(swaagerConfig));
+  try {
+    const swaggerConfig = readFileSync(path.join(__dirname, '../../swagger.json'), 'utf8');
+    SwaggerModule.setup('swagger', app, JSON.parse(swaggerConfig));
+  } catch (e){
+    const swaggerConfig = readFileSync(path.join(__dirname, '../../../swagger.json'), 'utf8');
+    SwaggerModule.setup('swagger', app, JSON.parse(swaggerConfig));
+  }
 }
