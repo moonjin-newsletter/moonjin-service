b7d48de2e9c0429ddbb53346157ae4d08021c166
Rotti_Kim <andy_1009@naver.com>
2024-03-21 22:25:54 +0900

    [hotfix] 폰트 체크

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -16,10 +16,11 @@ module.exports = {
       tablet: { min: "601px", max: "1023px" },
       desktop: { min: "1024px" },
     },
-
+    fontFamily: {
+      sans: ["Pretendard", "-apple-system", "sans-serif"],
+    },
     extend: {
       fontFamily: {
-        sans: ["Pretendard", "-apple-system", "sans-serif"],
         libre: ["var(--font-libre)"],
         serif: [
           "var(--font-noto-serif)",
