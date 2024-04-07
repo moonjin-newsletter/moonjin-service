9f9a4c1e1a6a834d55e807a4047ace1325ba4bfc
Rotti_Kim <andy_1009@naver.com>
2024-04-07 19:14:28 +0900

    [fix] 메일 전송 반영5

--- apps/client/app/mypage/(reader)/letter/_components/LetterTab.tsx
+++ apps/client/app/mypage/(reader)/letter/_components/LetterTab.tsx
@@ -77,9 +77,7 @@ function SendLetterCard({ letter }: { letter: LetterWithUserDto }) {
   return (
     <Link
       href={`/mypage/letter/send-${letter.id}`}
-      className={`${
-        letter.readAt && "bg-grayscale-100"
-      } flex flex-col w-full border-b border-grayscale-200 py-4 hover:bg-grayscale-700/10 rounded`}
+      className={`flex flex-col w-full border-b border-grayscale-200 py-4 hover:bg-grayscale-700/10 rounded`}
     >
       <div className="w-full flex items-center ">
         <I.LetterOut />
@@ -104,7 +102,9 @@ function ReceivedLetterCard({ letter }: { letter: LetterWithUserDto }) {
   return (
     <Link
       href={`/mypage/letter/receive-${letter.id}`}
-      className="flex flex-col w-full border-b border-grayscale-200 py-4 hover:bg-grayscale-700/10 rounded"
+      className={`${
+        letter.readAt && "bg-grayscale-100"
+      } flex flex-col w-full border-b border-grayscale-200 py-4 hover:bg-grayscale-700/10 rounded`}
     >
       <div className="w-full flex items-center ">
         <I.LetterIn />
