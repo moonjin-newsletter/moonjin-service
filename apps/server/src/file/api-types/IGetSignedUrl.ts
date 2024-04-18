6e2de7bcdc866094947f381e192ecb7db3e5b3d7
comeintovlanc <rkdtjdwns916@gmail.com>
2024-04-18 22:57:35 +0900

    [fix] 파일 이름 128자 까지 가능

--- apps/server/src/file/api-types/IGetSignedUrl.ts
+++ apps/server/src/file/api-types/IGetSignedUrl.ts
@@ -1,8 +1,7 @@
 import {FileTypeEnum} from "../enum/fileType.enum";
 import {tags} from "typia";
 
-
 export interface IGetSignedUrl {
     fileType: FileTypeEnum;
-    fileName: string & tags.MaxLength<32>;
+    fileName: string & tags.MaxLength<128>;
 }
