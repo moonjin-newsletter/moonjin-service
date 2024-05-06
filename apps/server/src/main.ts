c9325c7cd83cb1a879268ca84bdfb8da4a5103d6
comeintovlanc <rkdtjdwns916@gmail.com>
2024-05-06 23:37:53 +0900

    [fix] CORS 에러 해결

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -10,10 +10,10 @@ async function bootstrap() {
   swaggerBoot(app);
   app.use(cookieParser());
   app.enableCors({
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204,
+    origin: ['http://localhost:3000', 'http://13.125.224.11:3000','https://moonjin-service.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    preflightContinue: false,
   });
 
   await app.listen(Number(process.env.SERVER_PORT));
