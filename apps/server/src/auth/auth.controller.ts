23f1481509d82a52242e66bf06ea7fcc0ed5c331
comeintovlanc <rkdtjdwns916@gmail.com>
2024-05-08 18:05:46 +0900

    [fix] cookie 생성 에러 해결4

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -40,7 +40,7 @@ export class AuthController {
     httpOnly: true,
     sameSite: 'none',
     secure: true,
-    domain: process.env.CLIENT_URL
+    domain: process.env.CLIENT_DOMAIN
   }: {}
 
   constructor(private readonly authService : AuthService,
