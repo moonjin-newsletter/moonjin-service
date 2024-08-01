3c52b8149d915f9cc4e33cba0e8706ef24e0b2de
Rotti_Kim <kimrotti6239@gmail.com>
2024-08-01 19:17:54 +0900

    [fix] : 캐러셀 컴포넌트 변경

--- apps/client/components/carousel/useCarousel.tsx
+++ apps/client/components/carousel/useCarousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { JSX, ReactElement, useEffect, useRef, useState } from "react";
+import { JSX, ReactElement, useEffect, useState } from "react";
 
 type UseCarouselProps = {
   items: any[];
@@ -17,45 +17,37 @@ type UseCarouselReturn = {
   nextEvent: () => void;
 };
 
+const Carousel = ({ children }: CarouselProps) => {
+  return (
+    <div className="flex w-full overflow-hidden h-full items-center">
+      <ul id="carousel" className="flex w-fit h-fit px-5">
+        {children}
+      </ul>
+    </div>
+  );
+};
+
 export default function useCarousel({
   items,
   width,
 }: UseCarouselProps): UseCarouselReturn {
-  const carouselRef = useRef<HTMLUListElement>(null);
   const [currCell, setCurrCell] = useState(0);
 
   const prevEvent = () => {
     setCurrCell((prev) => (prev === 0 ? items.length - 1 : prev - 1));
-    if (carouselRef.current !== null) {
-      carouselRef.current.style.transition = "all 2s ease-in-out";
-    }
   };
 
   const nextEvent = () => {
     setCurrCell((prev) => (prev === items.length - 1 ? 0 : prev + 1));
-    if (carouselRef.current !== null) {
-      carouselRef.current.style.transition = "all 2s ease-in-out";
-    }
   };
 
   useEffect(() => {
-    if (carouselRef.current) {
-      // carouselRef.current.style.transition = "all 2s ease-in-out";
-      carouselRef.current.style.transform = `translateX(-${
-        currCell * width
-      }px) `;
+    const carouselRef = document?.getElementById("carousel");
+    if (carouselRef) {
+      carouselRef.style.transition = "all 0.5s ease-in-out";
+      carouselRef.style.transform = `translateX(-${currCell * width}px) `;
     }
   }, [currCell]);
-
-  const Carousel = ({ children }: CarouselProps) => {
-    return (
-      <div className="flex w-full overflow-hidden h-full items-center">
-        <ul ref={carouselRef} className="flex w-fit h-fit px-5">
-          {children}
-        </ul>
-      </div>
-    );
-  };
 
   return { Carousel, prevEvent, nextEvent };
 }
