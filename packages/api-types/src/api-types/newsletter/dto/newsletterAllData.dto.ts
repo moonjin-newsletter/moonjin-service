f5b3afc6aabfe04e897553f4b980e6b9516861bc
comeintovlanc <rkdtjdwns916@gmail.com>
2024-08-18 20:07:36 +0900

    [fix] newsletterAllData 타입 수정 반영

--- packages/api-types/src/api-types/newsletter/dto/newsletterAllData.dto.ts
+++ packages/api-types/src/api-types/newsletter/dto/newsletterAllData.dto.ts
@@ -1,4 +1,10 @@
-import {NewsletterCardDto} from "./newsletterCard.dto";
 import {PostWithContentDto} from "../../post/dto";
+import { WriterProfileDto} from "../../writerInfo/dto";
+import {SeriesDto} from "../../series/dto";
+import {NewsletterDto} from "./newsletter.dto";
 
-export interface NewsletterAllDataDto extends NewsletterCardDto, PostWithContentDto{}
+export interface NewsletterAllDataDto extends PostWithContentDto{
+    newsletter : NewsletterDto,
+    series : SeriesDto | null,
+    writer : WriterProfileDto
+}
