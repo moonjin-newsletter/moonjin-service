6caf41eacb846f98f9b191257c91930ca45be9a4
Rotti_Kim <kimrotti6239@gmail.com>
2024-09-24 11:05:17 +0900

    [feat] : 메타데이터 추가

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -3,11 +3,6 @@ import type { Metadata, Viewport } from "next";
 import { Libre_Baskerville, Noto_Serif_KR } from "next/font/google";
 import ClientProvider from "./ClientProvider";
 
-export const metadata: Metadata = {
-  title: "moonjin",
-  description: "작가와 독자의 연결",
-};
-
 const libre = Libre_Baskerville({
   subsets: ["latin"],
   display: "swap",
@@ -21,6 +16,30 @@ const notoSerifKR = Noto_Serif_KR({
   variable: "--font-noto-serif",
   weight: ["200", "300", "400", "500", "600", "700", "900"],
 });
+
+export const metadata: Metadata = {
+  title: "moonjin",
+  description:
+    "어제, 오늘 그리고 내일의 이야기가 담긴 문진에서 뉴스레터로 여러분의 이야기를 전달하세요.",
+  openGraph: {
+    type: "website",
+    title: "어제",
+    description: "어제, 오늘 그리고 내일의 이야기, 문진",
+    url: "https://dev.moonjin.site",
+    siteName: "moonjin",
+    images: "https://moonjin.site/og-image.png",
+  },
+  category: "newsletter",
+  keywords: ["뉴스레터", "문진", "글", "이야기"],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+  },
+
+  appleWebApp: {
+    capable: true,
+  },
+};
 
 export const viewport: Viewport = {
   width: "device-width",
