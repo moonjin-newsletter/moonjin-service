9e43e8da10d6d4b4ecd00d21effd65e9119284b7
Rotti_Kim <kimrotti6239@gmail.com>
2024-09-24 11:17:35 +0900

    [feat] : 메타데이터 변경

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -23,8 +23,8 @@ export const metadata: Metadata = {
     "어제, 오늘 그리고 내일의 이야기가 담긴 문진에서 뉴스레터로 여러분의 이야기를 전달하세요.",
   openGraph: {
     type: "website",
-    title: "어제",
-    description: "어제, 오늘 그리고 내일의 이야기, 문진",
+    title: "moonjin",
+    description: "어제, 오늘 그리고 내일의 이야기, 문진 뉴스레터",
     url: "https://dev.moonjin.site",
     siteName: "moonjin",
     images: "https://moonjin.site/og-image.png",
