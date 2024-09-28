5b6827da5e4fe3e266dafffd2e163c09057ed49b
Rotti_Kim <kimrotti6239@gmail.com>
2024-09-29 02:30:40 +0900

    [feat] : emailFooter 추가

---
+++ packages/editorjs/src/template/footers/emailFooter.ts
@@ -0,0 +1,187 @@
+export function EmailFooter() {
+  return `<tr>
+      <td>
+        <table
+          role="presentation"
+          border="0"
+          cellpadding="0"
+          cellspacing="0"
+          width="100%"
+          style="
+            max-width: 600px;
+            margin: 0 auto;
+            background-color: #ffffff;
+            border-radius: 0 0 24px 24px;
+            padding: 0 40px 24px 40px;
+          "
+        >
+          <tbody>
+            <tr>
+              <td align="center">
+                <table
+                  role="presentation"
+                  border="0"
+                  cellpadding="0"
+                  cellspacing="0"
+                  width="100%"
+                  style="
+                    max-width: 600px;
+                    margin: 0 auto;
+                    background-color: #f7f7f7;
+                    border-radius: 12px;
+                    padding: 40px;
+                  "
+                >
+                  <tbody>
+                    <tr align="center">
+                      <td>
+                        <img
+                          src="https://d1ppxineti4knh.cloudfront.net/static/email/footer_logo.png"
+                          alt="Moonjin"
+                          width="183"
+                          height="69"
+                          style="width: 148; height: 56px"
+                        />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td height="12"></td>
+                    </tr>
+                    <tr align="center">
+                      <td>
+                        <a
+                          href="https://moonjin.site/"
+                          target="_blank"
+                          style="color: #7b0000"
+                          >moonjin 바로가기</a
+                        >
+                      </td>
+                    </tr>
+                    <tr>
+                      <td height="24"></td>
+                    </tr>
+                    <tr align="center">
+                      <td>
+                        <span style="line-height: 1.5rem">
+                          문진을 이용하시는 데에 문제가 있으신가요?<br />
+                          언제든지 저희에게 문의해 주세요.
+                        </span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td height="12"></td>
+                    </tr>
+                    <tr align="center">
+                      <td>
+                        <a
+                          href="https://pf.kakao.com/_xhQgIn"
+                          target="_blank"
+                          style="color: #7b0000"
+                          >문의하기</a
+                        >
+                      </td>
+                    </tr>
+                    <tr>
+                      <td height="24"></td>
+                    </tr>
+                    <tr align="center">
+                      <td>
+                        <table
+                          role="presentation"
+                          border="0"
+                          cellpadding="0"
+                          cellspacing="0"
+                          width="176px"
+                        >
+                          <tbody>
+                            <tr align="center">
+                              <td style="width: 32px">
+                                <a
+                                  href="https://moonjin.site/"
+                                  target="_blank"
+                                  style="display: block"
+                                >
+                                  <img
+                                    src="https://d1ppxineti4knh.cloudfront.net/static/email/footer_web.png"
+                                    alt="Moonjin"
+                                    width="32"
+                                    height="32"
+                                    style="width: 32px; height: 32px"
+                                  />
+                                </a>
+                              </td>
+                              <td style="width: 32px">
+                                <a
+                                  href="https://pf.kakao.com/_xhQgIn"
+                                  target="_blank"
+                                  style="display: block"
+                                >
+                                  <img
+                                    src="https://d1ppxineti4knh.cloudfront.net/static/email/footer_kakao.png"
+                                    alt="Moonjin"
+                                    width="32"
+                                    height="32"
+                                    style="width: 32px; height: 32px"
+                                /></a>
+                              </td>
+                              <td style="width: 32px">
+                                <a
+                                  href="https://www.instagram.com/moonjin_official"
+                                  target="_blank"
+                                  style="display: block"
+                                >
+                                  <img
+                                    src="https://d1ppxineti4knh.cloudfront.net/static/email/footer_instargram.png"
+                                    alt="Moonjin"
+                                    width="32"
+                                    height="32"
+                                    style="width: 32px; height: 32px"
+                                  />
+                                </a>
+                              </td>
+                              <td style="width: 32px">
+                                <a
+                                  href="https://www.threads.net/@moonjin_official"
+                                  target="_blank"
+                                  style="display: block"
+                                >
+                                  <img
+                                    src="https://d1ppxineti4knh.cloudfront.net/static/email/footer_threads.png"
+                                    alt="Moonjin"
+                                    width="32"
+                                    height="32"
+                                    style="width: 32px; height: 32px"
+                                  />
+                                </a>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td height="12"></td>
+                    </tr>
+                    <tr align="center">
+                      <td>
+                        <span style="font-size: 10px; color: #999999"
+                          >© 2024 moonjin. All rights reserved.</span
+                        >
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td height="20"></td>
+    </tr>
+  </tbody>
+</table>
+`;
+}
