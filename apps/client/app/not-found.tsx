cdff645757e3903feff260b3ded70137d22b6cb4
Rotti_Kim <kimrotti6239@gmail.com>
2024-09-30 15:24:35 +0900

    [fix] : not-found 페이지 변경

--- apps/client/app/not-found.tsx
+++ apps/client/app/not-found.tsx
@@ -7,12 +7,6 @@ export default function Page() {
       <span className="text-grayscale-600 mt-4">
         서비스를 점검 중입니다. 잠시 후 다시 시도해주세요.
       </span>
-      {/*<Link*/}
-      {/*  href={"/"}*/}
-      {/*  className="py-1.5 px-3 text-sm bg-grayscale-700 text-white rounded mt-2 font-medium"*/}
-      {/*>*/}
-      {/*  문의하기*/}
-      {/*</Link>*/}
     </main>
   );
 }
