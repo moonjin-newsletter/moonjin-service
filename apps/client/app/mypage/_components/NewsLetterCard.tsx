7c81ec5526109751cb8467664f6ae78a60e17378
Rotti_Kim <kimrotti6239@gmail.com>
2024-10-12 23:51:14 +0900

    [fix] : 뉴스레터 카드 디자인 및 이미지 수정

--- apps/client/app/mypage/_components/NewsLetterCard.tsx
+++ apps/client/app/mypage/_components/NewsLetterCard.tsx
@@ -2,8 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { format } from "date-fns";
 import type { NewsletterCardDto } from "@moonjin/api-types";
-import { BiLike } from "react-icons/bi";
-import { FaRegCommentDots } from "react-icons/fa";
+import { LogoSymbolGray } from "@components/icons";
 
 export default function NewsLetterCard({
   newsletterInfo,
@@ -24,19 +23,21 @@ export default function NewsLetterCard({
         <h2 className="group-hover:underline mt-1.5 text-lg text-grayscale-600 font-medium">
           {newsletterInfo.post.title}
         </h2>
-        <div className="mt-0.5 w-full h-full grow flex flex-col gap-y-2">
-          <div className=" w-full flex  text-sm text-grayscale-400">
-            <span className="line-clamp-2">{newsletterInfo.post.subtitle}</span>
+        <div className="mt-1 w-full h-full grow flex flex-col gap-y-2">
+          <div className=" w-full flex text-sm text-grayscale-400">
+            <span className="line-clamp-2 font-light">
+              {newsletterInfo.post.subtitle}
+            </span>
           </div>
-          <div className="mt-auto flex items-center gap-x-3 text-[#999999] text-sm">
+          <div className="mt-auto flex items-center gap-x-4 text-[#999999] text-sm">
             <div className="flex items-center gap-x-1 ">
-              <BiLike />
+              <LogoSymbolGray width="18" height="18" viewBox="0 0 24 24" />
               <span>{newsletterInfo.newsletter.likes}</span>
             </div>
-            <div className="flex items-center gap-x-1">
-              <FaRegCommentDots />
-              <span>{newsletterInfo.newsletter.comments}</span>
-            </div>
+            {/*<div className="flex items-center gap-x-1">*/}
+            {/*  <FaRegCommentDots />*/}
+            {/*  <span>{newsletterInfo.newsletter.comments}</span>*/}
+            {/*</div>*/}
             <div className="flex items-center gap-x-1.5">
               <span>발행일자</span>
               <span>
@@ -54,7 +55,7 @@ export default function NewsLetterCard({
         alt="뉴스레터 커버이미지"
         width={120}
         height={120}
-        className="w-[120px] h-[120px] min-w-[120px] rounded"
+        className="w-[120px] h-[120px] min-w-[120px] rounded object-cover border border-grayscale-100"
       />
     </Link>
   );
